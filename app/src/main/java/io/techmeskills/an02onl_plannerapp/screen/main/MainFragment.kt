73ed15fe3660c2eceb032ba9e6afd2efccea707d
KirillVolkov <kirill.volkov.by@gmail.com>
2021-04-02 11:35:00 +0300

    feature/add_new_note

--- app/src/main/java/io/techmeskills/an02onl_plannerapp/screen/main/MainFragment.kt
+++ app/src/main/java/io/techmeskills/an02onl_plannerapp/screen/main/MainFragment.kt
@@ -2,10 +2,16 @@ package io.techmeskills.an02onl_plannerapp.screen.main
 
 import android.os.Bundle
 import android.view.View
+import androidx.activity.OnBackPressedCallback
+import androidx.fragment.app.setFragmentResultListener
+import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import io.techmeskills.an02onl_plannerapp.R
 import io.techmeskills.an02onl_plannerapp.databinding.FragmentMainBinding
+import io.techmeskills.an02onl_plannerapp.screen.add_new.AddNewFragment
 import io.techmeskills.an02onl_plannerapp.support.NavigationFragment
+import io.techmeskills.an02onl_plannerapp.support.navigateSafe
+import io.techmeskills.an02onl_plannerapp.support.setVerticalMargin
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainFragment : NavigationFragment<FragmentMainBinding>(R.layout.fragment_main) {
@@ -16,12 +22,34 @@ class MainFragment : NavigationFragment<FragmentMainBinding>(R.layout.fragment_m
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        
-        viewBinding.recyclerView.adapter = NotesRecyclerViewAdapter(viewModel.notes)
+
+        viewModel.notesLiveData.observe(this.viewLifecycleOwner, {
+            viewBinding.recyclerView.adapter = NotesRecyclerViewAdapter(it)
+        })
+
+        setFragmentResultListener(AddNewFragment.ADD_NEW_RESULT) { key, bundle ->
+            val note = bundle.getString(AddNewFragment.TEXT)
+            val date = bundle.getString(AddNewFragment.DATE)
+            note?.let {
+                viewModel.addNote(it, date)
+            }
+        }
+
+        viewBinding.btnAdd.setOnClickListener {
+            findNavController().navigateSafe(MainFragmentDirections.toAddNewFragment())
+        }
     }
 
     override fun onInsetsReceived(top: Int, bottom: Int, hasKeyboard: Boolean) {
         viewBinding.toolbar.setPadding(0, top, 0, 0)
         viewBinding.recyclerView.setPadding(0, 0, 0, bottom)
+        viewBinding.btnAdd.setVerticalMargin(marginBottom = bottom)
     }
+
+    override val backPressedCallback: OnBackPressedCallback
+        get() = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                findNavController().popBackStack()
+            }
+        }
 }
--- app/src/main/java/io/techmeskills/an02onl_plannerapp/screen/main/MainViewModel.kt
+++ app/src/main/java/io/techmeskills/an02onl_plannerapp/screen/main/MainViewModel.kt
@@ -1,26 +1,40 @@
 package io.techmeskills.an02onl_plannerapp.screen.main
 
+import androidx.lifecycle.MutableLiveData
 import io.techmeskills.an02onl_plannerapp.support.CoroutineViewModel
+import kotlinx.coroutines.launch
 
 class MainViewModel : CoroutineViewModel() {
 
-    val notes = listOf(
-        Note("Помыть посуду"),
-        Note("Забрать пальто из химчистки", "23.03.2021"),
-        Note("Позвонить Ибрагиму"),
-        Note("Заказать перламутровые пуговицы"),
-        Note("Избить соседа за шум ночью"),
-        Note("Выпить на неделе с Володей", "22.03.2021"),
-        Note("Починить кран"),
-        Note("Выбить ковры перед весной"),
-        Note("Заклеить сапог жене"),
-        Note("Купить картошки"),
-        Note("Скачать кино в самолёт", "25.03.2021")
+    val notesLiveData = MutableLiveData<List<Note>>(
+        listOf(
+            Note("Помыть посуду"),
+            Note("Забрать пальто из химчистки", "23.03.2021"),
+            Note("Позвонить Ибрагиму"),
+            Note("Заказать перламутровые пуговицы"),
+            Note("Избить соседа за шум ночью"),
+            Note("Выпить на неделе с Володей", "22.03.2021"),
+            Note("Починить кран"),
+            Note("Выбить ковры перед весной"),
+            Note("Заклеить сапог жене"),
+            Note("Купить картошки"),
+            Note("Скачать кино в самолёт", "25.03.2021")
+        )
     )
 
+    fun addNote(text: String, date: String? = null) {
+        launch {
+            val list = notesLiveData.value!!.toMutableList()
+            list.add(0, Note(text, date))
+            notesLiveData.postValue(list)
+        }
+    }
 }
 
 class Note(
     val title: String,
     val date: String? = null
 )
+
+
+
